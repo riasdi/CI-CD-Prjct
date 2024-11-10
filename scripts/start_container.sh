189aee05c8d74361211a2d81398661b5d00b923e
RivaD <158500289+riasdi@users.noreply.github.com>
2024-11-10 18:50:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rivadias/python-app
+docker pull rivadias/python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rivadias/python-app
+docker run -d -p 5000:5000 rivadias/python-flask-app
 
